b8797648416980ea0cbec2f91c5bec282cabeb86
DuckPC <krisaka8235@gmail.com>
2026-01-22 10:59:17 +0700

    test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/backend/:path*",
+                destination: "http://api:4491/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
