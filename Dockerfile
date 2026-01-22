99a52d3cf425a3f79dcefa5d56c5e4e8ab8a44b1
ThanhDat2505 <phanthanhdat2505@gmail.com>
2026-01-22 16:02:18 +0700

    test

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM node:20-alpine AS deps
 WORKDIR /app
-COPY package*.json ./
-RUN npm ci
+
+COPY package.json ./
+COPY package-lock.json* ./
+
+RUN if [ -f package-lock.json ]; then npm ci; else npm install; fi
 
 FROM node:20-alpine AS build
 WORKDIR /app
@@ -16,6 +19,7 @@ ENV NODE_ENV=production
 COPY --from=build /app/package.json ./package.json
 COPY --from=build /app/node_modules ./node_modules
 COPY --from=build /app/.next ./.next
+# Nếu project có public/ (thường có) thì giữ dòng này
 COPY --from=build /app/public ./public
 
 EXPOSE 3000
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
+const API_HOST =
+    process.env.API_PROXY_TARGET ??
+    (process.env.DOCKER === "true" ? "http://api:4491" : "http://localhost:4491");
+
 const nextConfig: NextConfig = {
-  /* config options here */
     async rewrites() {
+        console.log(`API_HOST: ${API_HOST}`);
         return [
             {
                 source: "/backend/:path*",
-                destination: "http://api:4491/:path*",
+                destination: `${API_HOST}/:path*`,
             },
         ];
     },
