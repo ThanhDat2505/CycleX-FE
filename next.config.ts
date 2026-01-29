43d54ead68575648ce5561983c9230010607fa4f
ThanhDat2505 <phanthanhdat2505@gmail.com>
2026-01-29 09:08:01 +0700

    pull main

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const API_HOST =
+    process.env.API_PROXY_TARGET ?? "http://localhost:4491";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: "/backend/:path*",
+                destination: `${API_HOST}/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
